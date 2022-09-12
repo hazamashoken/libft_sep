893abb340e1d1dc784f87b8b4779b8218633373c
tliangso <earth78203@gmail.com>
2022-09-12 20:27:49 +0800

    add ft_lstiter.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 10:16:47 by tliangso          #+#    #+#             */
-/*   Updated: 2022/09/12 20:15:45 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/09/12 20:24:02 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,6 +77,7 @@ t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
 void	ft_lstdelone(t_list *lst, void (*del)(void*));
 void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 // Extra Functions
 #endif
