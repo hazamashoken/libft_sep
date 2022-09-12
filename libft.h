06b99a18d9d6a1e65724b5902eaee4e81a474ca0
tliangso <earth78203@gmail.com>
2022-09-12 20:06:15 +0800

    add ft_lstlast.c

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 10:16:47 by tliangso          #+#    #+#             */
-/*   Updated: 2022/09/12 19:55:45 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/09/12 20:06:06 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,7 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+
 // Extra Functions
 #endif
