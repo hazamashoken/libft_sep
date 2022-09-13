bda9a5c0bb6e064e700e096b064feb5a0ece8231
tliangso <earth78203@gmail.com>
2022-09-13 10:52:04 +0800

    fix sstrncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/09 12:11:57 by tliangso          #+#    #+#             */
-/*   Updated: 2022/09/13 10:22:00 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/09/13 10:51:43 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	if (n == 0)
 		return (0);
 	i = 0;
-	while (i < n && *(s1 + i) && *(s2 + i))
+	while (i < n && *(s1 + i) && *(s1 + i) == *(s2 + i))
 		i++;
 	return (*(s1 + i) - *(s2 + i));
 }
