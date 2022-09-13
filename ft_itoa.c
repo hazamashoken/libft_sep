10dd5cb3c30f70f237a2c857a5461a12e95c6e36
tliangso <earth78203@gmail.com>
2022-09-13 20:34:31 +0800

    fix itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: tliangso <earth78203@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/11 10:13:40 by tliangso          #+#    #+#             */
-/*   Updated: 2022/09/13 20:32:31 by tliangso         ###   ########.fr       */
+/*   Updated: 2022/09/13 20:34:19 by tliangso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,21 @@
 
 #include	"libft.h"
 
-size_t	numlen(int n)
+size_t	numlen(long nb)
 {
 	size_t	len;
 
 	len = 0;
-	if (n == 0)
+	if (nb == 0)
 		return (1);
-	if (n < 0)
+	if (nb < 0)
 	{
-		n = -n;
+		nb = -nb;
 		len++;
 	}
-	while (n > 9)
+	while (nb > 9)
 	{
-		n /= 10;
+		nb /= 10;
 		len++;
 	}
 	return (++len);
@@ -48,24 +48,24 @@ char	*ft_itoa(int n)
 {
 	size_t	len;
 	size_t	i;
-	int		nb;
+	long	nb;
 	char	*res;
 
 	nb = n;
-	len = numlen(n);
+	len = numlen(nb);
 	res = malloc(sizeof(char) * (len + 1));
 	if (res == NULL)
 		return (NULL);
 	i = 0;
-	if (nb < 0)
-		n = -n;
+	if (n < 0)
+		nb = -nb;
 	while (i < len)
 	{
-		*(res + (len - i - 1)) = (n % 10) + '0';
-		n /= 10;
+		*(res + (len - i - 1)) = (nb % 10) + '0';
+		nb /= 10;
 		i++;
 	}
-	if (nb < 0)
+	if (n < 0)
 		*res = '-';
 	*(res + len) = '\0';
 	return (res);
